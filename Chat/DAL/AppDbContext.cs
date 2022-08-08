edfd70b86f58409d57391f12e0b969173b26d397
CavadovT <turalkhj@code.edu.az>
2022-08-08 05:39:22 +0400

    Add project files.

---
+++ Chat/DAL/AppDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Chat.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Chat.DAL
+{
+    public class AppDbContext:IdentityDbContext<AppUser>
+    {
+        public AppDbContext(DbContextOptions options):base(options)
+        {
+
+        }
+        public DbSet<Message> Messages { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Message>()
+                .HasOne(u => u.Recipient)
+                .WithMany(m => m.MessageReceived)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Message>()
+                .HasOne(u => u.Sender)
+                .WithMany(m => m.MessagesSent)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+
+    }
+}
---
+++ Chat/Extentions/ApplicationServiceExtentions.cs
@@ -0,0 +1,17 @@
+﻿using Chat.DAL;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Chat.Extentions
+{
+    public static class ApplicationServiceExtentions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config) 
+        {
+            services.AddDbContext<AppDbContext>(opt => { 
+            opt.UseSqlServer(config.GetConnectionString("De"))
+            })
+        }
+    }
+}
